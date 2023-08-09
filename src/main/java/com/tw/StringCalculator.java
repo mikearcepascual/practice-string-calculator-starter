d6309e079e6ec19757e499fea5108ff7493a03a9
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-09 09:16:32 +0800

    (Java-Exam): added enhancement to add function in StringCalculator

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,20 @@
 package com.tw;
+import java.lang.String;
 
 public class StringCalculator {
     public int add(String string) {
+        if(!string.isEmpty()) {
+            int sum = 0;
+            for(String s : string.split("[,\n;]|[^0-9]")){
+                if(!s.isEmpty()){
+                    sum += Integer.parseInt(s.trim());
+                }
+            }
+            return sum;
+        }
         return 0;
     }
+
+
 }
+
